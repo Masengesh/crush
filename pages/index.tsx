30b3cfb57461af2891d1cb77db04bcb0ac77a9b4
MACBOOK PRO <bientobienvenu@gmail.com>
2026-01-11 08:47:54 +0300

    footr1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import Button from '../components/Button';
 import Card from '../components/Card';
 import { apiClient } from '../utils/api';
